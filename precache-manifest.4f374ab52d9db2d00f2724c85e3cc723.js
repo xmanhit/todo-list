0cdf4afaf799cc519e68c16f76724b852d35a647
nxmanhit <nxmanh102@gmail.com>
2020-02-15 18:00:43 +0700

    Updates

---
+++ precache-manifest.4f374ab52d9db2d00f2724c85e3cc723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "519dbfb999291769bb6487e6dddff253",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "6fec7d1974163bd1f7e3",
+    "url": "/todo-list/static/css/main.7e502b78.chunk.css"
+  },
+  {
+    "revision": "211329f241f2ba2f6f6e",
+    "url": "/todo-list/static/js/2.a3cc6b38.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list/static/js/2.a3cc6b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fec7d1974163bd1f7e3",
+    "url": "/todo-list/static/js/main.b7c37913.chunk.js"
+  },
+  {
+    "revision": "3c8bc7aa72310e7d0f4e",
+    "url": "/todo-list/static/js/runtime-main.901ffa1a.js"
+  }
+]);
