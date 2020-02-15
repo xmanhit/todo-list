0072cfa4b10467b58969cad4c6ca4f68aa59d3a7
nxmanhit <nxmanh102@gmail.com>
2020-02-15 19:33:36 +0700

    update TodoForm

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,24 +3,26 @@ import React, { Component } from 'react';
 class TodoForm extends Component {
   constructor(props) {
     super(props);
+    this.form = React.createRef();
+    this.inputName = React.createRef();
     this.onSubmit = this.onSubmit.bind(this);
   }
   componentDidMount() {
-    this.refs.itemName.focus();
+    this.inputName.current.focus();
   }
   onSubmit(event) {
     event.preventDefault();
-    var newItemValue = this.refs.itemName.value;
+    var newItemValue = this.inputName.current.value;
     
     if(newItemValue) {
       this.props.addItem({newItemValue});
-      this.refs.form.reset();
+      this.form.reset();
     }
   }
   render () {
     return (
-      <form ref="form" onSubmit={this.onSubmit} className="form-inline">
-        <input type="text" ref="itemName" className="form-control" placeholder="add a new todo..."/>
+      <form ref={form => this.form = form} onSubmit={this.onSubmit} className="form-inline">
+        <input type="text" ref={this.inputName} className="form-control" placeholder="add a new todo..."/>
         <button type="submit" className="btn btn-default">Add</button> 
       </form>
     );   
